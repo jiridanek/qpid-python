41e9fb16cd68a6faa4f283752443b94922796790
Jiri Daněk <jdanek@redhat.com>
2023-04-10 18:27:03 +0200

    PY3__cmp__

--- qpid/datatypes.py
+++ qpid/datatypes.py
@@ -20,6 +20,7 @@
 from __future__ import absolute_import
 import threading, struct, datetime, time
 from .exceptions import Timeout
+from .py3compat import PY3__cmp__, long, cmp
 
 class Struct:
 
@@ -124,6 +125,7 @@ def serial(o):
   else:
     return Serial(o)
 
+@PY3__cmp__
 class Serial:
 
   def __init__(self, value):
@@ -290,6 +292,7 @@ try:
   from uuid import uuid4
   from uuid import UUID
 except ImportError:
+  @PY3__cmp__
   class UUID:
     def __init__(self, hex=None, bytes=None):
       if [hex, bytes].count(None) != 1:
